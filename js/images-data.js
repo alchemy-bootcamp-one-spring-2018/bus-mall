7e3d1a550c938bf397e5425d193d4878fbb88309
MariahAdams <mariah.adams23@yahoo.com>
2018-05-15 15:46:51 -0700

    added object literals for images

--- js/images-data.js
+++ js/images-data.js
@@ -3,34 +3,148 @@
 'use strict';
 
 //list of images
-let imageArray = [
-    new Image ('bag', 'bag.png'),
-    new Image ('banana', 'banana.png'),
-    new Image ('bathroom', 'bathroom.png'),
-    new Image ('boots', 'boots.png'),
-    new Image ('breakfast', 'breakfast.png'),
-    new Image ('bubblegum', 'bubblegum.png'),
-    new Image ('chair', 'chair.png'),
-    new Image ('cthulhu', 'cthulhu.png'),
-    new Image ('dog-duck', 'dog-duck.png'),
-    new Image ('dragon', 'dragon.png'),
-    new Image ('pen', 'pen.png'),
-    new Image ('pet-sweep', 'pet-sweep.png'),
-    new Image ('scissors', 'scissors.png'),
-    new Image ('shark', 'shark.png'),
-    new Image ('sweep', 'sweep.png'),
-    new Image ('tauntaun', 'tauntaun.png'),
-    new Image ('unicorn', 'unicorn.png'),
-    new Image ('usb', 'usb.png'),
-    new Image ('water-can', 'water-can.png'),
-    new Image ('wine-glass', 'wine-glass.png')
-];
+// let imageArray = [
+//     new Image ('bag', 'bag.png'),
+//     new Image ('banana', 'banana.png'),
+//     new Image ('bathroom', 'bathroom.png'),
+//     new Image ('boots', 'boots.png'),
+//     new Image ('breakfast', 'breakfast.png'),
+//     new Image ('bubblegum', 'bubblegum.png'),
+//     new Image ('chair', 'chair.png'),
+//     new Image ('cthulhu', 'cthulhu.png'),
+//     new Image ('dog-duck', 'dog-duck.png'),
+//     new Image ('dragon', 'dragon.png'),
+//     new Image ('pen', 'pen.png'),
+//     new Image ('pet-sweep', 'pet-sweep.png'),
+//     new Image ('scissors', 'scissors.png'),
+//     new Image ('shark', 'shark.png'),
+//     new Image ('sweep', 'sweep.png'),
+//     new Image ('tauntaun', 'tauntaun.png'),
+//     new Image ('unicorn', 'unicorn.png'),
+//     new Image ('usb', 'usb.png'),
+//     new Image ('water-can', 'water-can.png'),
+//     new Image ('wine-glass', 'wine-glass.png')
+// ];
 
 let images = [
     {
-        name: 
-        imageSrc:
-        views:
-        votes:
+        name: 'bag',
+        imageSrc: 'bag.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'banana',
+        imageSrc: 'banana.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'bathroom',
+        imageSrc: 'bathroom.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'boots',
+        imageSrc: 'boots.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'breakfast',
+        imageSrc: 'breakfast.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'bubblegum',
+        imageSrc: 'bubblegum.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'chair',
+        imageSrc: 'chair.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'cthulhu',
+        imageSrc: 'cthulhu.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'dog-duck',
+        imageSrc: 'dog-duck.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'dragon',
+        imageSrc: 'dragon.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'pen',
+        imageSrc: 'pen.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'pet-sweep',
+        imageSrc: 'pet-sweep.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'scissors',
+        imageSrc: 'scissors.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'shark',
+        imageSrc: 'shark.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'sweep',
+        imageSrc: 'sweep.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'tauntaun',
+        imageSrc: 'tauntaun.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'unicorn',
+        imageSrc: 'unicorn.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'usb',
+        imageSrc: 'usb.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'water-can',
+        imageSrc: 'water-can.png',
+        views: 0,
+        votes: 0
+    },
+    {
+        name: 'wine-glass',
+        imageSrc: 'wine-glass.png',
+        views: 0,
+        votes: 0
     }
-]
+];
