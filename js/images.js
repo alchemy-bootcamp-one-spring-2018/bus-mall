7cd54b427199719f3ab309ef13b8a9f36edff8aa
Mark Myers <markalope@gmail.com>
2018-05-15 14:17:14 -0700

    Tested object constructor.

--- js/images.js
+++ js/images.js
@@ -2,13 +2,17 @@ document.write('images.js is loading properly');
 
 const productOption = [
     {
-    product: url('img/bag.jpg');
-    viewed: 0;
-    votes: 0;
+    name: 'R2-D2 Roller Bag',
+    image: 'bag.jpg',
+    viewed: 0,
+    votes: 0
     },
     {
-        product: url('img/bag.jpg');
-        viewed: 0;
-        votes: 0; 
+    name: 'Banana Slicer',
+    image: 'banana.jpg',
+    viewed: 0,
+    votes: 0
     }
-]
+];
+console.log(productOption);
+console.log(productOption[1].name);
