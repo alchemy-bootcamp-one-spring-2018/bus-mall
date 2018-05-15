63897138bad749fbcec9f4489f9ed45affe4d871
Zachar13y <zachar13y@gmail.com>
2018-05-15 14:58:15 -0700

    Random calculator is working!

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+// holds the images
+
+const imageArray = [1, 2, 3, 4, 5];
+
+// gets random item out of the image array
+
+
+const randomImg = imageArray[Math.floor(Math.random() * imageArray.length)];
+
+console.log(randomImg);
+
+// function grabImage() {
+//     for(let i = 0; i < imageArray.length; i++);
+// }
+
+// grabImage();
+
+
+
