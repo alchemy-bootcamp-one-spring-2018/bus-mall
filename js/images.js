095073356d5451e6477284b5302cf60f948b0f36
Mark Myers <markalope@gmail.com>
2018-05-15 14:38:36 -0700

    Populated all image objects. Tested in console.

--- js/images.js
+++ js/images.js
@@ -1,18 +1,147 @@
-document.write('images.js is loading properly');
+//document.write('images.js is loading properly');
 
 const productOption = [
     {
-    name: 'R2-D2 Roller Bag',
-    image: 'bag.jpg',
-    viewed: 0,
-    votes: 0
+        id: 1,
+        name: 'R2-D2 Roller Bag',
+        image: 'bag.jpg',
+        viewed: 0,
+        votes: 0
     },
     {
-    name: 'Banana Slicer',
-    image: 'banana.jpg',
-    viewed: 0,
-    votes: 0
+        id: 2,
+        name: 'Banana Slicer',
+        image: 'banana.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 3,
+        name: 'Bathroom Tablet Stand',
+        image: 'bathroom.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 4,
+        name: 'Open Toe Rain Boots',
+        image: 'boots.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 5,
+        name: 'All-in-One Breakfast Center',
+        image: 'breakfast.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 6,
+        name: 'Italian Meatball Bubble Gum',
+        image: 'bubblegum.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 7,
+        name: 'Bubble Seat Chair',
+        image: 'chair.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 8,
+        name: 'Raging Cthulhu Figure',
+        image: 'cthulhu.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 9,
+        name: 'Dog Duckbill',
+        image: 'dog-duck.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 10,
+        name: 'Dragon Meat',
+        image: 'dragon.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 11,
+        name: 'Utensil Pen Caps',
+        image: 'pen.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 12,
+        name: 'Pet Sweep',
+        image: 'pet-sweep.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 13,
+        name: 'Pizza Scissors',
+        image: 'scissors.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 14,
+        name: 'Shark Sleeping Bag',
+        image: 'shark.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 15,
+        name: 'Baby Sweeper Onesie',
+        image: 'sweep.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 16,
+        name: 'Tauntaun Sleeping Bag',
+        image: 'tauntaun.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 17,
+        name: 'Unicorn Meat',
+        image: 'unicorn.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 18,
+        name: 'USB Twitching Tentacle',
+        image: 'usb.gif',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 19,
+        name: 'Impossible Watering Can',
+        image: 'water-can.jpg',
+        viewed: 0,
+        votes: 0
+    },
+    {
+        id: 20,
+        name: 'Impossible Wine Glass',
+        image: 'wine-glass.jpg',
+        viewed: 0,
+        votes: 0
     }
 ];
+
 console.log(productOption);
-console.log(productOption[1].name);
+console.log(productOption[5].name);
