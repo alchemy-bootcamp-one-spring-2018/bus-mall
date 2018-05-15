d392828c6204961a2444b1f4e8faf56fe9a51a04
Mark Myers <markalope@gmail.com>
2018-05-15 14:44:56 -0700

    Created and tested random number generator

--- js/randomizer.js
+++ js/randomizer.js
@@ -1,1 +1,9 @@
 document.write('randomizer.js is loading properly');
+
+/* globals productOption */
+/*exported displaySets */
+
+function randomNumber(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+console.log(randomNumber(99));
