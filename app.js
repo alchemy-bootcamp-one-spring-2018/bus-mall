8bb0cd09bef9c30df47a0db45d173f9e7d432da4
Easton Gorishek <ieaston26@gmail.com>
2018-05-15 15:19:57 -0700

    Have index.html, main.js and app.js talking to each other

---
+++ app.js
@@ -0,0 +1,14 @@
+/* exported */
+const appTemplate = document.getElementById('app-template');
+
+class App {
+    constructor() {
+
+    }
+
+    render() {
+        const testing = appTemplate.content;
+        return testing;
+    }
+}
+
