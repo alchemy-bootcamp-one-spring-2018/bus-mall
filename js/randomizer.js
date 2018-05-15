9854509bb9ff3c7b1ab3669e5e94852401afc558
Mark Myers <markalope@gmail.com>
2018-05-15 14:56:00 -0700

    working on image set selector

--- js/randomizer.js
+++ js/randomizer.js
@@ -3,7 +3,21 @@ document.write('randomizer.js is loading properly');
 /* globals productOption */
 /*exported displaySets */
 
+var selectedImage = '';
+
 function randomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+function selectImage(productOption) {
+    let index = randomNumber(productOption.length);
+    let selectedImage = productOption[index];
+    productOption.splice(index, 1);
+    return selectedImage;
+}
+
+
+// Console logs for testing
 console.log(randomNumber(99));
+console.log(productOption.length);
+console.log(selectedImage);
