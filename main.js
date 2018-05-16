e15bf89debc02f9cbb9c0ea24e4b7600f4540261
Rehmsy <sarahrehmer@gmail.com>
2018-05-16 13:37:13 -0700

    moved array with item name and src to separate image-array.js

--- main.js
+++ main.js
@@ -1,40 +1,33 @@
-var myImage = new Image();
-myImage.src = 'images/banana.jpg';
-document.getElementById('my-images').appendChild(myImage);
 
-function createImg() {
-    var img = document.createElement('img');
-    img.setAttribute('src', 'images/bag.jpg');
-    document.body.appendChild(img);
-}
 
-var imageArray = [
-    { name: 'bag', image: 'images/bag.jpg' },
-    { name: 'banana', image: 'images/banana.jpg' },
-    { name: 'bathroom', image: 'images/bathroom.jpg' },
-    { name: 'boots', image: 'images/boots.jpg' },
-    { name: 'breakfast', image: 'images/breakfast.jpg' },
-    { name: 'bubblegum', image: 'images/bubblegum.jpg' },
-    { name: 'chair', image: 'images/chair.jpg' },
-    { name: 'cthulhu', image: 'images/cthulhu.jpg' },
-    { name: 'dog duck', image: 'images/dog-duck.jpg' },
-    { name: 'dragon', image: 'images/dragon.jpg' },
-    { name: 'pen', image: 'images/pen.jpg' },
-    { name: 'pet sweep', image: 'images/pet-sweep.jpg' },
-    { name: 'scissors', image: 'images/scissors.jpg' },
-    { name: 'shark', image: 'images/shark.png' },
-    { name: 'sweep', image: 'images/sweep.png' },
-    { name: 'tauntaun', image: 'images/tauntaun.jpg' },
-    { name: 'unicorn', image: 'images/unicorn.jpg' },
-    { name: 'usb', image: 'images/usb.gif' },
-    { name: 'water can', image: 'images/water-can.jpg' },
-    { name: 'wine glass', image: 'images/wine-glass.jpg' }
-];
+
 console.log(imageArray);
 
-function randomNumber(amount) {
-    return Math.floor(Math.random(amount) * imageArray.length);
+class App {
+        constructio() {
+            this.name = name;
+            this.image = image;
+        }        
 }
+
+class ImageViewer {
+    constructor(image) {
+        render
+    }
+}
+
+//function randomNumber(amount) {
+  //  return Math.floor(Math.random(amount) * imageArray.length);
+//}
+// var myImage = new Image();
+// myImage.src = 'images/banana.jpg';
+// document.getElementById('my-images').appendChild(myImage);
+
+// function createImg() {
+//     var img = document.createElement('img');
+//     img.setAttribute('src', 'images/bag.jpg');
+//     document.body.appendChild(img);
+// }
 
 
 function getRandomImage(imageArray) {
