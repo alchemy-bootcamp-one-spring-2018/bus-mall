d02de3baa99eaabfc8802c78615d1f30f11154f9
Antreo Pukay <acrosswalker@yahoo.com>
2018-05-15 18:09:38 -0700

    troubleshooting splice issue

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
 
 
-const imagesArray = ['images/bag.jpg', 'images/banana.jpg', 'images/bathroom.jpg', 'images/boots.jpg', 'images/breakfast.jpg', 'images/bubblegum.jpg', 'images/chair.jpg', 'images/cthulhu.jpg', 'images/dog-duck.jpg', 'images/dragon.jpg', 'images/pen.jpg', 'images/pet-sweep.jpg', 'images/scissors.jpg', 'images/shark.jpg', 'images/sweep.png', 'images/tauntaun.jpg', 'images/unicorn.jpg', 'images/usb.gif', 'images/water-can.jpg', 'images/wine-glass.jpg']; 
 
 
 
 for(var i = 0; i < 3; i++) {
+    const imagesArray = ['images/bag.jpg', 'images/banana.jpg', 'images/bathroom.jpg', 'images/boots.jpg', 'images/breakfast.jpg', 'images/bubblegum.jpg', 'images/chair.jpg', 'images/cthulhu.jpg', 'images/dog-duck.jpg', 'images/dragon.jpg', 'images/pen.jpg', 'images/pet-sweep.jpg', 'images/scissors.jpg', 'images/shark.jpg', 'images/sweep.png', 'images/tauntaun.jpg', 'images/unicorn.jpg', 'images/usb.gif', 'images/water-can.jpg', 'images/wine-glass.jpg'];
+    
+    var randomImage = Math.floor((Math.random() * imagesArray.length) + 1);
+    console.log('number', randomImage);
 
-    var randomImage = Math.floor((Math.random() * 20) + 1);
-    
-    
     var displayImageSet = document.getElementById('images');
     var imageSet = document.createElement('img');
     imageSet.setAttribute('src', imagesArray[randomImage]);
     displayImageSet.appendChild(imageSet);
     imageSet.style.height = '200px';
+    imagesArray.splice(randomImage, 1);
 }
-
 //function randomImages(imagesArray) {
     
 
