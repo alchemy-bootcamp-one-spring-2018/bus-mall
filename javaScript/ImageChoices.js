7d41cb08a6b9af342c06a0736c3c29e89808e411
Chris Golden <chrgolden@gmail.com>
2018-05-15 18:15:00 -0700

    displaying 3 images

--- javaScript/ImageChoices.js
+++ javaScript/ImageChoices.js
@@ -1,4 +1,5 @@
-/* exported ImageObjects */
+/* exported ImageChoices */
+/* globals objectArray */
 'use strict';
 
 const votingTemplate = document.getElementById('voting-template');
@@ -10,9 +11,19 @@ class ImageChoices {
 
     render() {
         const dom = votingTemplate.content.cloneNode(true);
-        // const span = dom.querySelector('span');
-        // const img = this.img;
-        // span.appendChild(img);
+        
+        const image1 = dom.getElementById('image-1');
+        console.log(objectArray[0].imgSrc);
+        image1.src = objectArray[0].imgSrc;
+
+        const image2 = dom.getElementById('image-2');
+        console.log(objectArray[2].imgSrc);
+        image2.src = objectArray[2].imgSrc;
+
+        const image3 = dom.getElementById('image-3');
+        console.log(objectArray[3].imgSrc);
+        image3.src = objectArray[3].imgSrc;
+        
         return dom;
     }
 }
