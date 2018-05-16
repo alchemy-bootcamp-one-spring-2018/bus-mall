ac43b4349276d15f5fc16e1bbd97e6fb48901378
Ryan Luras <ryanluras@gmail.com>
2018-05-15 21:47:29 -0700

    added my product list of items that i know ill need

---
+++ js/app.js
@@ -0,0 +1,26 @@
+/* exported App */
+
+const appTemplate = document.getElementById('app-template');
+
+class App {
+    constructor() {
+
+    }
+    
+    render() {
+        const dom = appTemplate.content;
+
+
+
+
+
+
+
+
+
+
+
+
+        return dom;
+    }
+}
