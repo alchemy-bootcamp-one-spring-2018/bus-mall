993f99e4cac2569e1f3ef788099094dc3ff4916f
Chris Golden <chrgolden@gmail.com>
2018-05-17 00:13:18 -0700

    got an object showing in results

--- javaScript/SurveyResult.js
+++ javaScript/SurveyResult.js
@@ -7,7 +7,10 @@ class SurveyResult {
     }
     render() {
         const dom = resultDataTemplate.content.cloneNode(true);
-
+        const column = dom.querySelectorAll('td');
+        column[0].textContent = this.object.name;
+        column[1].textContent = this.object.views;
+        column[2].textContent = this.object.clicks;
         return dom;
     }
 
