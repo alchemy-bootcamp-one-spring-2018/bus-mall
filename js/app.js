89129b590ee7ddd3fdc76d92fe404a529a66286c
rbtprograms <rbt61690@gmail.com>
2018-05-17 14:45:49 -0700

    views working! and total views!

--- js/app.js
+++ js/app.js
@@ -5,18 +5,26 @@ const appTemplate = document.getElementById('app-template').content;
 class App {
     constructor() {
         this.items = items;
+        this.totalViews = 0;
     }
 
     render() {
         const dom = appTemplate;
 
         const displayImageSelection = dom.getElementById('displayed-images');
-        const imageViewerComponent = new ItemDisplay(acquireRandomItems(), (item) => {
+        const imageViewerComponent = new ItemDisplay(acquireRandomItems(), (item,) => {
         //My this.update was inside of here, but it never worked.
             item.votes++;
             console.log(item.name + ' has ' + item.votes + ' votes');
             imageViewerComponent.update(acquireRandomItems());
+            this.totalViews++;
+            if(this.totalViews >= 25){
+                alert('good job');
+            }
         });
+
+        
+        
         const imageDom = imageViewerComponent.render();
         displayImageSelection.appendChild(imageDom);
 
