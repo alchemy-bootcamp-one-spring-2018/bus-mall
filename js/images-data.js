0076af3ce5f1491c214e01fec2e78d0139ea5279
MariahAdams <mariah.adams23@yahoo.com>
2018-05-17 16:28:01 -0700

    local storage hooked up

--- js/images-data.js
+++ js/images-data.js
@@ -1,153 +1,150 @@
-/* exported imagesData */
+/* exported imagesData,  */
 /* globals */
 'use strict';
 
-let imagesData = [
-    {
-        name: 'bag',
-        imageSrc: 'img/bag.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'banana',
-        imageSrc: 'img/banana.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'bathroom',
-        imageSrc: 'img/bathroom.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'boots',
-        imageSrc: 'img/boots.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'breakfast',
-        imageSrc: 'img/breakfast.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'bubblegum',
-        imageSrc: 'img/bubblegum.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'chair',
-        imageSrc: 'img/chair.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'cthulhu',
-        imageSrc: 'img/cthulhu.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'dog-duck',
-        imageSrc: 'img/dog-duck.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'dragon',
-        imageSrc: 'img/dragon.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'pen',
-        imageSrc: 'img/pen.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'pet-sweep',
-        imageSrc: 'img/pet-sweep.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'scissors',
-        imageSrc: 'img/scissors.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'shark',
-        imageSrc: 'img/shark.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'sweep',
-        imageSrc: 'img/sweep.png',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'tauntaun',
-        imageSrc: 'img/tauntaun.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'unicorn',
-        imageSrc: 'img/unicorn.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'usb',
-        imageSrc: 'img/usb.gif',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'water-can',
-        imageSrc: 'img/water-can.jpg',
-        views: 0,
-        votes: 0
-    },
-    {
-        name: 'wine-glass',
-        imageSrc: 'img/wine-glass.jpg',
-        views: 0,
-        votes: 0
-    }
-];
+const ProductsData = window.localStorage.getItem('imagesData');
+
+window.onbeforeunload = () => {
+    window.localStorage.setItem('imagesData', JSON.stringify(imagesData));
+};
+
+let imagesData;
+if(ProductsData) {
+    imagesData = JSON.parse(ProductsData);
+}
+else {
+    initImages();
+}
+
+
+function initImages() {
+    imagesData = [
+        {
+            name: 'bag',
+            imageSrc: 'img/bag.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'banana',
+            imageSrc: 'img/banana.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'bathroom',
+            imageSrc: 'img/bathroom.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'boots',
+            imageSrc: 'img/boots.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'breakfast',
+            imageSrc: 'img/breakfast.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'bubblegum',
+            imageSrc: 'img/bubblegum.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'chair',
+            imageSrc: 'img/chair.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'cthulhu',
+            imageSrc: 'img/cthulhu.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'dog-duck',
+            imageSrc: 'img/dog-duck.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'dragon',
+            imageSrc: 'img/dragon.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'pen',
+            imageSrc: 'img/pen.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'pet-sweep',
+            imageSrc: 'img/pet-sweep.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'scissors',
+            imageSrc: 'img/scissors.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'shark',
+            imageSrc: 'img/shark.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'sweep',
+            imageSrc: 'img/sweep.png',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'tauntaun',
+            imageSrc: 'img/tauntaun.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'unicorn',
+            imageSrc: 'img/unicorn.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'usb',
+            imageSrc: 'img/usb.gif',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'water-can',
+            imageSrc: 'img/water-can.jpg',
+            views: 0,
+            votes: 0
+        },
+        {
+            name: 'wine-glass',
+            imageSrc: 'img/wine-glass.jpg',
+            views: 0,
+            votes: 0
+        }
+    ];
+}
 
 console.log(imagesData);
 
-//NOT DELETING THIS IN CASE I CHANGE MY MIND AND SWITCH APPROACHES
-//list of images - keeping this just in case
-// let imageArray = [
-//     new Image ('bag', 'bag.jpg'),
-//     new Image ('banana', 'banana.png'),
-//     new Image ('bathroom', 'bathroom.png'),
-//     new Image ('boots', 'boots.png'),
-//     new Image ('breakfast', 'breakfast.png'),
-//     new Image ('bubblegum', 'bubblegum.png'),
-//     new Image ('chair', 'chair.png'),
-//     new Image ('cthulhu', 'cthulhu.png'),
-//     new Image ('dog-duck', 'dog-duck.png'),
-//     new Image ('dragon', 'dragon.png'),
-//     new Image ('pen', 'pen.png'),
-//     new Image ('pet-sweep', 'pet-sweep.png'),
-//     new Image ('scissors', 'scissors.png'),
-//     new Image ('shark', 'shark.png'),
-//     new Image ('sweep', 'sweep.png'),
-//     new Image ('tauntaun', 'tauntaun.png'),
-//     new Image ('unicorn', 'unicorn.png'),
-//     new Image ('usb', 'usb.png'),
-//     new Image ('water-can', 'water-can.png'),
-//     new Image ('wine-glass', 'wine-glass.png')
-// ];
+// function clearProductsData() {
+//     window.localStorage.clear('imagesData');
+//     initImages();
+// }
