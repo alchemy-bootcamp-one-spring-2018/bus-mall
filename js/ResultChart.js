72264aa2d701431551d158374e40f9f06f390e12
Arthur Jen <arthurljen@gmail.com>
2018-05-17 18:00:17 -0700

    tweaking data visualization

--- js/ResultChart.js
+++ js/ResultChart.js
@@ -24,23 +24,40 @@ class ResultChart {
         }
 
         this.chart = new Chart(ctx, {
-            type: 'pie',
+            type: 'doughnut',
             data: {
                 labels: labels,
                 datasets: [{
                     label: '# of Clicks',
                     data: data,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)'
+                        'rgba(255, 0, 0, 1)',
+                        'rgba(255, 77, 0, 1)',
+                        'rgba(255, 153, 0, 1)',
+                        'rgba(204, 255, 0, 1)',
+                        'rgba(128, 255, 0, 1)',
+                        'rgba(51, 255, 0, 1)',
+                        'rgba(0, 255, 26, 1)',
+                        'rgba(0, 255, 102, 1)',
+                        'rgba(0, 255, 179, 1)',
+                        'rgba(0, 255, 255, 1)',
+                        'rgba(0, 179, 255, 1)',
+                        'rgba(0, 102, 255, 1)',
+                        'rgba(0, 26, 255, 1)',
+                        'rgba(51, 0, 255, 1)',
+                        'rgba(128, 0, 255, 1)',
+                        'rgba(204, 0, 255, 1)',
+                        'rgba(255, 0, 230, 1)',
+                        'rgba(255, 0, 153, 1)',
+                        'rgba(255, 0, 76, 1)',
+                        'rgba(255, 0, 0, 1)'
                     ],
                     borderColor: [
                         'rgba(255,99,132,1)',
                         'rgba(54, 162, 235, 1)',
                         'rgba(255, 206, 86, 1)'
                     ],
-                    borderWidth: 1
+                    borderWidth: 0
                 }]
             },
             options: {
