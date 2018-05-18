4ef280218c5f9b6799c83066d6d07543d6464c61
Rehmsy <sarahrehmer@gmail.com>
2018-05-18 00:00:35 -0700

    no progress.

--- ShowImageTemplate.js
+++ ShowImageTemplate.js
@@ -1,24 +1,22 @@
-/* exported ShowImages */
+/* exported Picture */
 
 const showImagesTemplate = document.getElementById('show-images-template');
 
-class Product {
-
-    constructor() {
-        this.arrayOfProducts = arrayOfProducts;
-        this.totalCount = 0;
-
+class Image {
+    constructor(image, onSelect) {
+        this.image = image;
+        this.onSelect = onSelect;
     }
 
     render() {
         const dom = showImagesTemplate.content.cloneNode(true);
-        const img = dom.querySelector('image');
-        img.src = this.arrayOfProducts.image;
+        const img = dom.querySelector('img');
+        img.src = this.arrayOfProducts.source;
         this.arrayOfProducts.viewCount++;
         img.addEventListener('click', () => {
             this.onSelect(this.arrayOfProducts);
         });
-
-        return dom; 
+        
+        return dom;
     }
 }
