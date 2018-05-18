5387e530387f968767b72dacc946222742c38f3c
Ryan Luras <ryanluras@gmail.com>
2018-05-17 23:27:03 -0700

    major overhaul, need to wire up results

--- js/ImageViewer.js
+++ js/ImageViewer.js
@@ -1,41 +1,34 @@
-/* globals */
-/* exported Vote */
+/* globals InputImage*/
+/* exported ImageViewer */
 'use strict';
 
-const imageViewerTemplate = document.getElementById('image-viewer-template').content;
+const imageViewerTemplate = document.getElementById('image-viewer-template');
 
 class ImageViewer {
-    constructor(image0, image1, image2, onImageVote) {
-        this.image0 = image0;
-        this.image1 = image1;
-        this.image2 = image2;
-        this.onImageVote = onImageVote;
-        
+    constructor(threeProducts, clicked) {
+        this.threeProducts = threeProducts;
+        this.clicked = clicked;
     }
 
+    update(imgArray) {
+        this.threeProducts = imgArray;
+        while(this.imageViewerSection.lastElementChild) {
+            this.imageViewerSection.lastElementChild.remove();
+        }
 
+        for(let i = 0; i < imgArray.length; i++) {
+            const imageComponent = new InputImage(imgArray[i], this.clicked);
+
+            this.imageViewerSection.appendChild(imageComponent.render());
+        }
+
+    }
     render() {
-        const dom = imageViewerTemplate.cloneNode(true);
         
-        const first = dom.getElementById('item-0');
-        first.src = this.image0.image;
-        first.addEventListener('click', () => {
-            this.onImageVote(this.image0.name);
-            
-        });
-        const second = dom.getElementById('item-1');
-        second.src = this.image1.image;
-        second.addEventListener('click', () => {
-            this.onImageVote(this.image1.name);
-        });
-        const third = dom.getElementById('item-2');
-        third.src = this.image2.image;
-        third.addEventListener('click', () => {
-            this.onImageVote(this.image2.name);
-        });
+        const dom = imageViewerTemplate.content.cloneNode(true);
+        this.imageViewerSection = dom.getElementById('image-viewer-section');
 
-
-
+        this.update(this.threeProducts);
         
         return dom;
     }
--- js/app.js
+++ js/app.js
@@ -6,53 +6,53 @@ class App {
     constructor() {
         this.productArray = productArray;
         this.totalVotes = 0;
-        console.log('totalVotes:', this.totalVotes);
-        
+        this.Array = [];
     }
-    
-    onImageVote(productName) {
+
+    update() {
+        while(this.imageViewer.lastElementChild) {
+            this.imageViewer.lastElementChild.remove();
+        }
+        const endSurvey = document.createElement('h2');
+        endSurvey.textContent = 'Thanks for participating!';
+        this.imageViewer.appendChild(endSurvey);
     }
-    
-    
+
     render() {
         const dom = document.getElementById('app-template').content;
-        
-        
-        const imageViewerSection = dom.getElementById('image-viewer');
-        
-        const randomIndex = threeRandomProducts(productArray.length);
-        const image0 = productArray[randomIndex[0]];
-        const image1 = productArray[randomIndex[1]];
-        const image2 = productArray[randomIndex[2]];
-        
-        const imageComponent = new ImageViewer(image0, image1, image2, (productName) => {
-            console.log('productName :', productName);
+        this.imageViewer = dom.getElementById('image-viewer');
+        const randomImageArray = this.threeRandomProducts(this.productArray.length);
+        const imageComponent = new ImageViewer(randomImageArray, (product) => {
+            product.clicks++;
             this.totalVotes++;
-            console.log('totalVotes:', this.totalVotes);
+            const randomImageArray = this.threeRandomProducts(this.productArray.length);
+            imageComponent.update(randomImageArray);
+            if(this.totalVotes === 25) {
+                this.update();
+            }
         });
-        
-        
-        
-        imageViewerSection.appendChild(imageComponent.render());
-        
-        
-        
-        
+        this.imageViewer.appendChild(imageComponent.render());
         return dom;
     }
-    
+
+    //I had major help with this part and its still rather confusing
+    threeRandomProducts(max) {
+        let indexArray = [];
+        let objectArray = [];
+        for(let i = 0; i < 3;) {
+            let int = randomInt(max);
+            if(this.Array.includes(int) === false && indexArray.includes(int) === false) {
+                indexArray[i] = int;
+                this.productArray[int].views++;
+                objectArray[i] = this.productArray[int];
+                i++;
+            }
+        }
+        this.Array = indexArray;
+        return objectArray;
+    }
 }
-
-
 
 function randomInt(max) {
     return Math.floor(Math.random() * max);
-}
-
-function threeRandomProducts(max) {
-    let arrayOfThreeProducts = [];
-    for(let i = 0; i < 3; i++) {
-        arrayOfThreeProducts[i] = randomInt(max);
-    }
-    return arrayOfThreeProducts;
 }
