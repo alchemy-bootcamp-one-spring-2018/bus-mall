db35a62cdfe0ebbfbafd532ba340b24a806895fb
Kevin Grayson <k.grayson.ms@gmail.com>
2018-05-17 17:59:55 -0700

    Local storage working

--- js/data.js
+++ js/data.js
@@ -14,26 +14,48 @@ class Image {
     }
 }
 
-const imageArray = [
-    new Image('Bag', 'img/bag.jpg'),
-    new Image('Banana', 'img/banana.jpg'),
-    new Image('Bathroom', 'img/bathroom.jpg'),
-    new Image('Boots', 'img/boots.jpg'),
-    new Image('Breakfast', 'img/breakfast.jpg'),
-    new Image('Bubblegum', 'img/bubblegum.jpg'),
-    new Image('Chair', 'img/chair.jpg'),
-    new Image('Cthulhu', 'img/cthulhu.jpg'),
-    new Image('Dog-Duck', 'img/dog-duck.jpg'),
-    new Image('Dragon', 'img/dragon.jpg'),
-    new Image('Pen', 'img/pen.jpg'),
-    new Image('Pet-Sweep', 'img/pet-sweep.jpg'),
-    new Image('Scissors', 'img/scissors.jpg'),
-    new Image('Shark', 'img/shark.jpg'),
-    new Image('Sweep', 'img/sweep.png'),
-    new Image('TaunTaun', 'img/tauntaun.jpg'),
-    new Image('Unicorn', 'img/unicorn.jpg'),
-    new Image('USB', 'img/usb.gif'),
-    new Image('Water Can', 'img/water-can.jpg'),
-    new Image('Wine Glass', 'img/wine-glass.jpg')
-];
+const productData = window.localStorage.getItem('imageArray');
 
+window.onbeforeunload = () => {
+    window.localStorage.setItem('imageArray', JSON.stringify(imageArray));
+};
+
+let imageArray;
+
+if(!productData) {
+    initImageArray();
+}
+else {
+    imageArray = JSON.parse(productData);
+}
+
+console.log('JSON PARSE ', JSON.parse(productData));
+
+function initImageArray() {
+
+    imageArray = [
+        new Image('Bag', 'img/bag.jpg'),
+        new Image('Banana', 'img/banana.jpg'),
+        new Image('Bathroom', 'img/bathroom.jpg'),
+        new Image('Boots', 'img/boots.jpg'),
+        new Image('Breakfast', 'img/breakfast.jpg'),
+        new Image('Bubblegum', 'img/bubblegum.jpg'),
+        new Image('Chair', 'img/chair.jpg'),
+        new Image('Cthulhu', 'img/cthulhu.jpg'),
+        new Image('Dog-Duck', 'img/dog-duck.jpg'),
+        new Image('Dragon', 'img/dragon.jpg'),
+        new Image('Pen', 'img/pen.jpg'),
+        new Image('Pet-Sweep', 'img/pet-sweep.jpg'),
+        new Image('Scissors', 'img/scissors.jpg'),
+        new Image('Shark', 'img/shark.jpg'),
+        new Image('Sweep', 'img/sweep.png'),
+        new Image('TaunTaun', 'img/tauntaun.jpg'),
+        new Image('Unicorn', 'img/unicorn.jpg'),
+        new Image('USB', 'img/usb.gif'),
+        new Image('Water Can', 'img/water-can.jpg'),
+        new Image('Wine Glass', 'img/wine-glass.jpg')
+    ];
+}
+
+
+    
