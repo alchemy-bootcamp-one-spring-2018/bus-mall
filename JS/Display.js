3684cdf4fa53c86f83cc9c7c33bad6e91e200bc6
Zachar13y <zachar13y@gmail.com>
2018-05-18 15:14:06 -0700

    Three pictures are back

--- JS/Display.js
+++ JS/Display.js
@@ -15,13 +15,11 @@ class Display {
         
         const imageSection = dom.getElementById('three-images');
         
+        // whenClicked() {
+
         for(let i = 0; i < 3; i++) {
             const selectedImage = new Selected(this.list[i]);
-            const whatever = selectedImage.object.path;
-            console.log(whatever);
-            // console.log(selectedImage.object.name);
             imageSection.appendChild(selectedImage.render());
-            console.log('in display.js');
         }
         return dom;
 
--- JS/clicked.js
+++ JS/clicked.js
@@ -12,15 +12,19 @@ class Selected {
 
     render() {
         const dom = clickedTemplate.cloneNode(true);
+        // const clickedPic = this.onClick(this.object);
             
-           
         const domImage = dom.querySelector('img');
-        // domImage.addEventListener('click', () => {
-        //     this.onClick(this.object);
-        //     console.log('in clicked.js');
-        // });
+        domImage.addEventListener('click', () => {
+            this.onClick(this.object);
+        });
+        
+
+        // for (let i = 0; i < 25, i++;) {
 
 
+        domImage.src = this.object.path;
         return dom;
+
     }
 }
