e31585a16dcc02fc43dae013a6d1b170e6fb3b5f
Rehmsy <sarahrehmer@gmail.com>
2018-05-20 10:47:26 -0700

    randomizer works, but splice not set up right

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
-
 /* globals arrayOfProducts, Product */
-/* exported App */
+/* exported App, imageDom */
+function getRandomProducts() {
+    let copy = arrayOfProducts.slice();
+    let holderArray = [];
+    for(var i = 0; i < 3; i++) {
+        var itemOne = getRandomItem(copy);
+        holderArray.push(itemOne);
+    }
+    return holderArray;
+}
 
-const appTemplate = document.getElementById('app-template').content;
+getRandomProducts();
+    
+    
+function getRandomItem(arrayOfProducts) {
+    let index = getRandomNumber(arrayOfProducts.length);
+    let item = arrayOfProducts[index];
+    arrayOfProducts.splice(index, 1);
+    return item;
+}
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+let appTemplate = document.getElementById('app-template').content;
 
 class App {
     constructor() {
         this.arrayOfProducts = arrayOfProducts;
         this.totalCount = 0;
     }
-
+    
     render() {
         const dom = appTemplate;
         
         const displayImageSection = dom.getElementById('display-image-section');
-        const imageViewerComponent = new Product(arrayOfProducts);
-        const imageDom = imageViewerComponent.render();
+        const imageViewerComponent = new Product(getRandomProducts(), (item) => {
+            imageViewerComponent.update(getRandomProducts());
+        
+        });
+        
+        const imageDom = imageViewerComponent.render(); 
         displayImageSection.appendChild(imageDom);
         
-        return dom;
-        
+        return dom; 
     }
 }
+    
+
+
+
+
